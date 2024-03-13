8be75370bcbcf51afedc694e5126464092d39925
Erik Junholm <erik.junholm@liu.se>
2024-03-13 14:22:57 +0000

    [FIX]

--- build_script.ps1
+++ build_script.ps1
@@ -1,10 +1,10 @@
 #mkdir PackedGame
 
 echo "Adding Unreal to path !"
-#$env:PATH += ";C:\Program Files\Epic Games\UE_5.1\Engine\Binaries\Win64"
+$env:PATH += ";C:\Program Files\Epic Games\UE_5.1\Engine\Binaries\Win64"
 
 echo "Adding Unreal to unreal automation tool  !"
-#$env:PATH += ";C:\Program Files\Epic Games\UE_5.1\Engine\Build\BatchFiles\"
+$env:PATH += ";C:\Program Files\Epic Games\UE_5.1\Engine\Build\BatchFiles\"
 
 
 RunUAT.bat BuildCookRun -project="C:\Gitlab-Runner\builds\RpajUN3z\0\Exploranation\City\digitaltwincity-plattform\digitaltwinnkpg-ue5\DigitalTwin.uproject" -noP4  -platform=Win64  -clientconfig=Development  -serverconfig=Development -cook -allmaps -build  -stage -pak -archive -archivedirectory="PackedGame" 
